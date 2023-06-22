1c8908fd3c6e6d8a745de97abe1174e8d52e4603
bobikenobi12 <borislav1207@gmail.com>
2023-06-22 11:20:02 +0300

    add skeleton for PeopleListPage

--- client/src/pages/PeopleListPage.tsx
+++ client/src/pages/PeopleListPage.tsx
@@ -1,13 +1,30 @@
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Flex, useColorModeValue, Skeleton } from "@chakra-ui/react";
 
 import PersonCard from "../components/PersonCard";
 
 import { useGetPeopleQuery } from "../features/people/peopleApiSlice";
 
+function PeopleListPageSkeleton() {
+	return (
+		<Flex
+			minH={"100vh"}
+			flexWrap="wrap"
+			justifyContent="center"
+			alignItems="center"
+			gap={6}
+			bg={useColorModeValue("gray.100", "gray.800")}
+		>
+			{Array.from(Array(5)).map((_, idx) => (
+				<Skeleton key={idx} width="300px" height="400px" />
+			))}
+		</Flex>
+	);
+}
+
 export default function PeopleListPage() {
 	const { data: people, isLoading } = useGetPeopleQuery();
 
-	if (isLoading) return <div>Зареждане...</div>;
+	if (isLoading) return <PeopleListPageSkeleton />;
 
 	return (
 		<Flex
